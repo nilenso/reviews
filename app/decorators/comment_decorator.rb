776511a978dc49181b130e4c22e86c543b8f5189
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-03-08 00:21:04 +0530

    Fix links and button colors

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -5,14 +5,14 @@ class CommentDecorator < Draper::Decorator
     renderer = Redcarpet::Render::HTML.new
     markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(model.body || "")
-  end
+  en
 
   def render_with_children(commentable, all_comments)
     children = all_comments[self.id]
-    buttons = [h.link_to("Reply", h.new_comment_path(commentable_id: commentable.id, commentable_type: commentable_type(commentable), parent_id: model.id), class: "comment-button btn btn-default btn-xs"),
-               h.link_to("Edit", h.edit_comment_path(model, commentable_id: commentable.id, commentable_type: commentable_type(commentable)), class: "comment-button btn btn-default btn-xs"),
-               h.link_to("Delete", h.comment_path(model, commentable_id: commentable.id, commentable_type: commentable_type(commentable)), method: :delete, data: {confirm: "Sure?"}, class: "comment-button btn btn-default btn-xs")].join
-    author = h.content_tag(:span, "-- " + model.user.name, class: "comment-author")
+    buttons = [h.link_to("Reply", h.new_comment_path(commentable_id: commentable.id, commentable_type: commentable_type(commentable), parent_id: model.id), class: "comment-button btn btn-dark btn-xs"),
+               h.link_to("Edit", h.edit_comment_path(model, commentable_id: commentable.id, commentable_type: commentable_type(commentable)), class: "comment-button btn btn-dark btn-xs"),
+               h.link_to("Delete", h.comment_path(model, commentable_id: commentable.id, commentable_type: commentable_type(commentable)), method: :delete, data: {confirm: "Sure?"}, class: "comment-button btn btn-dark btn-xs")].join
+    author = h.content_tag(:span, "-- " + model.user.name, class:"comment-author")
     if children.present?
       children = children.map { |child| child.render_with_children(commentable, all_comments) }
       h.content_tag(:li, "".html_safe + h.content_tag(:div, self.markdown_text.html_safe, class: "comment") + author.html_safe + buttons.html_safe + h.content_tag(:ul, children.join.html_safe) )
